91dfe1403185605270bc2b7ca867ff5659bb26c8
Elimasrojas <elimasrojas@gmail.com>
2023-03-03 22:11:45 -0500

    product_entity

---
+++ src/main/java/com/elr/elr/domain/ProductStatus.java
@@ -0,0 +1,5 @@
+package com.elr.elr.domain;
+
+public enum ProductStatus {
+    NEW,IN_STOCK,DISCONTINUED
+}
---
+++ src/test/java/com/elr/elr/repositories/ProductRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.elr.elr.repositories;
+
+import com.elr.elr.domain.Product;
+import com.elr.elr.domain.ProductStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles("local")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ProductRepositoryTest {
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void testSaveProduct() {
+        Product product = new Product();
+        product.setDescription("nuevo producto");
+        product.setProductStatus(ProductStatus.NEW);
+
+        Product saveProduct = productRepository.save(product);
+        Product fetchedProduct= productRepository.getReferenceById(saveProduct.getId());
+        assertNotNull(fetchedProduct);
+        assertNotNull(fetchedProduct.getDescription());
+        assertNotNull(fetchedProduct.getCreatedDate());
+        assertNotNull(fetchedProduct.getLastModifiedDate());
+    }
+
+}
