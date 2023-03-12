bd9f2669504c7f5a624b26a0ee5ca90e082f10a8
Elimasrojas <elimasrojas@gmail.com>
2023-03-11 19:29:39 -0500

    153. Lazy Initialize Error

--- src/main/java/com/elr/elr/bootstrap/Bootstrap.java
+++ src/main/java/com/elr/elr/bootstrap/Bootstrap.java
@@ -1,15 +1,36 @@
 package com.elr.elr.bootstrap;
 
+import com.elr.elr.domain.OrderHeader;
+import com.elr.elr.repositories.OrderHeaderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
+    @Autowired
+    OrderHeaderRepository orderHeaderRepository;
+
     /*
     * SE EJECUTA E INICIA CON SPING CONTEXT
     * */
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("I was called!");
+        //System.out.println("I was called!");
+        OrderHeader orderHeader= orderHeaderRepository.findById(1L).get();
+        orderHeader.getOrderLines().forEach(ol -> {
+            System.out.println(ol.getProduct().getDescription());
+            /*
+            * Caused by: org.hibernate.LazyInitializationException: failed to lazily initialize a collection of role:
+            *  com.elr.elr.domain.Product.categories: could not initialize proxy - no Session
+            * necesita @Transacional
+
+            * */
+            ol.getProduct().getCategories().forEach(cat ->{
+                System.out.println(cat.getDescription() );
+            });
+        });
     }
 }
